436457d3c23e1e8271b2b8eaef33a9cbad91bdca
Sam Hall <mann.samantha11@gmail.com>
2020-11-05 08:28:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,14 +15,14 @@ end
 
 def take_a_number(deli, new_customer)
   deli << new_customer
-  puts "Welcome, #{new_customer}. You are number #{deli.length} in line."
+  puts "Welcome, #{new_customer}. You are number #{deli.length} in line." 
 end
 
-def now_serving(line)
-  if line.length == 0
+def now_serving(deli)
+  if deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}."
-    line.shift
+    deli.shift
   end
 end
